bfad829bc25ca4cb757ab260a3e56f656898f4d0
Daniel <danieldebrito2105@gmail.com>
2025-05-12 21:35:52 -0300

    Primeiras páginas

---
+++ project/competicaoPPLApp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    # return HttpResponse("Hello, World!")
+    return render(request, "competicaoPPLApp/index.html")
+
+def parte1(request):
+    """Renders the page for part 1 of the competition."""
+    # For now, just render a placeholder template
+    return render(request, "competicaoPPLApp/parte1.html")
